e6647464354b7f3b2053ba0a2e9d0d8ed1539509
Scorpio <diallomikey13@yahoo.fr>
2018-09-25 16:13:32 -0400

    final commit

--- assets/javascript/GifTastic.js
+++ assets/javascript/GifTastic.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    var cartoons = ["Looney Tunes", "Tom and Jerry", "The Ren & Stimpy Show"];
-
+    var cartoons = ["Looney Tunes", "Tom and Jerry", "The Ren & Stimpy Show", "Ed, Edd, & Eddy", "Dexter's Laboratory", "Tex Avery", "Beavis and Butt-head", "Animaniacs"];
+    //function to add a button by typing in the search bar
     function addButton() {
         $("#buttonContainer").empty();
         for (var i = 0; i < cartoons.length; i++) {
@@ -16,10 +16,10 @@ $(document).ready(function () {
         cartoons.push(cartoon);
         addButton();
     });
-
+    //function to dispay gifs when user clicks on gif buttons
     function displayGif() {
         var cartoon = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+cartoon+"&limit=10&api_key=HYE43IlOgwOMW76N5xykZtSWpK2YzIer";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+cartoon+"&limit=10&api_key=HYE43IlOgwOMW76N5xykZtSWpK2YzIer&rating=g";
 
         $.ajax({
             url: queryURL,
@@ -31,7 +31,7 @@ $(document).ready(function () {
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
                 var gifElement = $("<img>");
-                gifElement.addClass("gif");
+                gifElement.addClass("gif img-fluid float-left img-thumbnail");
                 gifElement.attr("src", results[i].images.fixed_height_still.url);
                 gifElement.attr("title", "Rating: " + results[i].rating);
                 gifElement.attr("data-still", results[i].images.fixed_height_still.url);
@@ -42,7 +42,7 @@ $(document).ready(function () {
             }
         });
     }
-
+    //function to animate or stop gifs when user clicks on them
     $(document).on("click", ".gif", function () {
         var state = $(this).attr("data-state");
         if (state == "still") {
